8371a04c1c0ab6eeaf153c79a8eeb06e49940b59
Mitsuru Nakakawaji <mitsuru@procube.jp>
2022-12-24 10:08:37 +0900

    avoid "resource busy"

--- restore-config.sh
+++ restore-config.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 supervisorctl stop slapd
 
 LDAP_CONFIG_BACKUP_FILE=$1
 echo restore ldap config
-rm -fr /etc/openldap/slapd.d
-mkdir -m 700 /etc/openldap/slapd.d
+rm -fr /etc/openldap/slapd.d/*
 slapadd -n0 -F /etc/openldap/slapd.d -l $LDAP_CONFIG_BACKUP_FILE
 chown ldap:ldap -R /etc/openldap/slapd.d
 
-supervisor start slapd
+supervisorctl start slapd
